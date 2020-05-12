4b634a67aa9294cb23265d01aaf9a374f72305b1
alina.timonina172372 <alina.timonina172372@gmail.com>
2020-05-12 15:24:26 +0300

    Добавьте файлы проекта.

---
+++ PP(Z4)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP_Z4_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите N: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Вызов функции: " + Func(x));
+            Console.ReadKey();
+        }
+        static double Func(int N, int Begin = 0)
+        {
+            if (Begin == 0)
+            {
+                return N / Math.Sqrt(Func(N, ++Begin));
+            }
+            else
+            {
+                return N == Begin ? N : Begin + Math.Sqrt(Func(N, ++Begin));
+            }
+        }
+    }
+}
---
+++ PP(Z4_1)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP_Z4_1_
+{
+    class Program
+    {
+        static void notation(int i, ref string s)
+        {
+            if (i > 0)
+            {
+                notation(i / 2, ref s);
+                s += (i % 2).ToString();
+            }
+            if (i == 0) Console.Write("");
+            if (i < 0) Console.Write("Проверьте число, которое хотите перевести!");
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число, которое хотите перевести: ");
+            int val_10 = int.Parse(Console.ReadLine());
+            string val_2 = "";
+            notation(val_10, ref val_2);
+            Console.Write("Число " + val_10 + " в двоичной сс = " + val_2);
+            Console.ReadKey();
+        }
+    }
+}
